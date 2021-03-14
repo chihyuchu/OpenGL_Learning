33a89d80eadf8261937b01b8f2fe08b076c43440
chihchu <chihchu@qti.qualcomm.com>
2021-03-14 16:55:52 +0800

    [The Cherno OpenGL]: Progress: 22/31 - Integrate ImGUI tool into OpenGL codebase

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -17,6 +17,9 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw_gl3.h"
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -90,7 +93,7 @@ int main(void)
 		// use sstream and fstream to read source shader file
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
-		shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
+		//shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
 		shader.SetUniformMat4f("u_MVP", mvp);
 		Texture texture("res/textures/love_icon.png");
 		texture.Bind(0);
@@ -103,8 +106,13 @@ int main(void)
 
 		Renderer render;
 
+		ImGui::CreateContext();    
+		ImGui_ImplGlfwGL3_Init(window, true);
+		ImGui::StyleColorsDark();
+
 		float r = 0.0f;
 		float increment = 0.01f;
+		glm::vec3 translation(-100.0f, 0.0f, 0.0f);
 
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window))
@@ -116,12 +124,28 @@ int main(void)
 				increment = 0.01f;
 			r += increment;
 
+			// ImGui new frame
+			ImGui_ImplGlfwGL3_NewFrame();
+			glm::mat4 view = glm::translate(glm::mat4(1.0f), translation);
+			glm::mat4 mvp = proj * view;
+
 			/* Render here */
 			render.Clear();
 
 			shader.Bind();
 			shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+			shader.SetUniformMat4f("u_MVP", mvp);
 			render.Draw(va, ib, shader);
+
+			{
+				// ImGui Float setting
+				ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);            
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			}
+
+			// Render ImGUi
+			ImGui::Render();
+			ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
 			/* Swap front and back buffers */
 			GLCall(glfwSwapBuffers(window));
@@ -133,6 +157,10 @@ int main(void)
 		
 		shader.Unbind();
 	}
+
+	// Cleanup ImGui
+	ImGui_ImplGlfwGL3_Shutdown();
+	ImGui::DestroyContext();
 
 	glfwTerminate();
 	
